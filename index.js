c56f94d7add55439ba87ac50080752a8dc631433
nassim oueghlani <n.oueghlani@gmail.com>
2021-03-01 16:39:22 +0100

    Back End Fonctionnel

--- index.js
+++ index.js
@@ -1,69 +1,83 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
+app.use(express.json());
 
-app.listen(85, ()=>{console.log('REST API via ExpressJS')})
+var corsOptions = {
+  origin: "*"
+};
 
-const MongoClient = require('mongodb').MongoClient;
+app.use(cors(corsOptions));
+
+app.listen(81, () => {
+  console.log("REST API via ExpressJS");
+});
+
+const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://127.0.0.1:27017";
 const dbName = "gestibank";
-let db
-MongoClient.connect(url,function(err,client){
-    console.log("Connexion réussie avec Mongo");
-    db = client.db(dbName);
-})
+let db;
+MongoClient.connect(url, function (err, client) {
+  console.log("Connexion réussie avec Mongo");
+  db = client.db(dbName);
+});
 
-app.get('/users', (req,res)=>{
-    db.collection('user').find({}).toArray(function(err, docs){
-        if (err) {
-            console.log(err)
-            throw (err)
-        }
-        res.status(200).json(docs)
-    })
-})
+app.get("/users", (req, res) => {
+  db.collection("user")
+    .find({})
+    .toArray(function (err, docs) {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      res.status(200).json(docs);
+    });
+});
 
-app.get('/users/:id', async (req,res)=>{
-    const id= parseInt(req.params.id)
-    try {
-        const docs = await db.collection('user').findOne({id})
-        res.status(200).json (docs)
-    } catch (err) {
-        console.log(err)
-        throw err
-    }
-})
+app.get("/users/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const docs = await db.collection("user").findOne({ id });
+    res.status(200).json(docs);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+});
 
-app.post('/users', async (req,res)=>{
-    try {
-        const userData = req.body
-        const user = await db.collection('user').inserOne(userData)
-        res.status(200).json(user)
-    } catch (err) {
-        console.log(err)
-        throw err
-    }
-})
+app.post("/users", async (req, res) => {
+  try {
+    const userData = req.body;
+    const user = await db.collection("user").insertOne(userData);
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+});
 
-app.put('/users/:id', async (req,res)=>{
-    try {
-        const id = parseInt(req.params.id)
-        const replacementUser = req.body
-        const user = await db.collection('user').replaceOne({id}, replacementUser)
-        res.status(200).json(user)
-    } catch (err){
-    console.log(err)
-    throw err 
-    }
-})
+app.put("/users/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const replacementUser = req.body;
+    const user = await db
+      .collection("user")
+      .replaceOne({ id }, replacementUser);
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+});
 
-app.delete ('/users/:id', async (req,res)=> {
-    try {
-        const id = parseInt(req.params.id)
-        const user = await db.collection('user').deleteOne({id})
-        res.status(200).json(user)
-    } catch (err) {
-        console.log(err)
-        throw err
-    }
-})
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const user = await db.collection("user").deleteOne({ id });
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+});
